d6af27d74c8ac3f426f16527d0dab363ddb7f269
PhyTeam <buibaphuc95@gmail.com>
2016-10-17 22:56:30 +0700

    Added information gain.

--- KDTree.py
+++ KDTree.py
@@ -3,18 +3,27 @@ from time import clock
 from collections import namedtuple
 from pprint import pformat
 from operator import itemgetter
+import os
+import numpy as np
 
-
-class Node(namedtuple('Node', 'location left_child right_child')):
+class Node(namedtuple('Node', 'location left_child right_child leaf_bucket')):
     def __repr__(self):
         return pformat(tuple(self))
 
 
-def kdtree(point_list, depth=0):
+def kdtree(point_list, depth=0, bucket_threshold=1):
     try:
         k = len(point_list[0])  # assumes all points have the same diamension
     except IndexError as e:
         return None
+    # Check point list has number of point less or equal threshold
+    n = len(point_list)
+    if n <= bucket_threshold:
+        return Node(
+            None,
+            None, None,
+            point_list
+        )
     # Select axis
     # FIXME: Choose longest diamension
     axis = depth % k
@@ -28,9 +37,16 @@ def kdtree(point_list, depth=0):
         right_child=kdtree(point_list[median + 1:], depth + 1)
     )
 
+def load_pendigits(url):
+    dataset = np.loadtxt(url, delimiter=',')
+    return dataset
+
 def main():
     """Example """
     point_list = [(2, 3), (2, 3), (5, 4), (9, 6), (4, 7), (8, 1), (7, 2)]
+    n = len(point_list)
+    k = len(point_list[0]) # Can cause error
+    bucket_threshold =  n / (10 * k) # FIXME: approriate 10 bucket for a cluster
     tree = kdtree(point_list)
     print tree
 
--- KMean.py
+++ KMean.py
@@ -1,7 +1,10 @@
 from numpy import *
 import random as rnd
 import matplotlib.pyplot as plt
+from collections import Counter
 MAX_ITERATIONS = 4
+
+
 def getRandomCentroids(dataset, k):
     numFeatures = 1
     numData = shape(dataset)[0]
@@ -9,22 +12,24 @@ def getRandomCentroids(dataset, k):
         numFeatures = shape(dataset)[1]
 
     # Assign k sample as centroids
-    return  array(rnd.sample(dataset, k))
+    return array(rnd.sample(dataset, k))
+
 
 def getCentroids(dataset, labels, k):
     newCentroids = []
     for xcluster in range(k):
         cluster = [i for i, j in zip(dataset, labels) if j == xcluster]
         newCentroids.append(mean(cluster, axis=0))
-    print "{0:-^30}".format("New Centrolds")
-    print newCentroids
+    # print "{0:-^30}".format("New Centrolds")
+    # print newCentroids
     return array(newCentroids)
 
 
 def distance(d1, d2):
     assert len(d1) == len(d2)
     d = len(d1)
-    return sqrt(sum([ (i - j) * (i - j) for i,j in zip(d1, d2)]))
+    return sqrt(sum([(i - j) * (i - j) for i, j in zip(d1, d2)]))
+
 
 def getLabels(dataset, centroids):
     k = len(centroids)
@@ -40,6 +45,7 @@ def getLabels(dataset, centroids):
         labels[dindex] = label
     return labels
 
+
 def shouldStop(oldCentroids, centroids, iterations):
     if iterations >= MAX_ITERATIONS:
         return True
@@ -47,24 +53,25 @@ def shouldStop(oldCentroids, centroids, iterations):
         return False
     return array_equal(oldCentroids, centroids)
 
-def kmeans(dataset, k):
 
+def kmeans(dataset, k):
     # Initialize centroids randomly ( Forgy approch)
     numFeatures = 1
     if ndim(dataset) > 1:
         numFeatures = shape(dataset)[1]
     centroids = getRandomCentroids(dataset, k)
-    print centroids
-    figure = plt.figure()
-    ax1 = figure.add_subplot(111)
+    # print centroids
+    # figure = plt.figure()
+    # ax1 = figure.add_subplot(111)
 
-    plt.plot(dataset[:,0], dataset[:,1], 'bo')
-    plt.plot(centroids[:,0], centroids[:, 1], 'w.')
+    # plt.plot(dataset[:, 0], dataset[:, 1], 'bo')
+    # plt.plot(centroids[:, 0], centroids[:, 1], 'w.')
 
     # plt.show()
 
     # Initialize book keeping vars
     iterations, oldCentroids = 0, None
+    labels = None
     while not shouldStop(oldCentroids, centroids, iterations):
         # Save old centroids
         oldCentroids = centroids
@@ -74,12 +81,11 @@ def kmeans(dataset, k):
         labels = getLabels(dataset, centroids)
 
         # Assign new centroids base on datapoint label
-        figure2 = plt.figure()
-        ax2 = figure2.add_subplot(111)
-
-
+        # figure2 = plt.figure()
+        # ax2 = figure2.add_subplot(111)
+        '''
         color = ['r', 'b', 'g', 'c', 'm', 'i', 'k', 'w']
-        for i in range(k): # For each
+        for i in range(k):  # For each
             # print labels
             cluster = []
             for j in range(len(dataset)):
@@ -88,18 +94,54 @@ def kmeans(dataset, k):
             if len(cluster) > 0:
                 cluster = array(cluster)
                 plt.plot(cluster[:, 0], cluster[:, 1], color[i] + 'o')
-        plt.plot(centroids[:, 0], centroids[:,1], 'k^')
+        plt.plot(centroids[:, 0], centroids[:, 1], 'k^')'''
         # Calculate new centroids
-        centroids = getCentroids(dataset, labels,k)
-    plt.show()
+        centroids = getCentroids(dataset, labels, k)
+    # plt.show()
+    return labels
+
+def calcInformationGain(dataset, outputs, targets):
+    classes = set(targets)
+    c = Counter(targets)
+    co = Counter(outputs)
+    t = Counter(zip(outputs, targets))
+    n = float(len(targets))
+    print c
+    print "N = ", n,
+    print classes
+    ENTotal = sum ([ - c[cls] / float(n) * log2(c[cls] / float(n) ) for cls in classes])
+    print ENTotal
+    SENk = 0
+    for k in classes:
+        ENk = 0
+        nk = float(co[k])
+        for cls in classes:
+            v = t[(k, cls)]
+
+            if v != 0:
+                ENk += -(v / nk) * (log2(v / nk))
+        w = nk / n
+        SENk += w * ENk
+    print "ENTotal = ", ENTotal
+    print "ENk = " , SENk
+    NIG = 1
+    if ENTotal != 0:
+        NIG = (ENTotal - SENk) / ENTotal
+    print "NIG = ", NIG
+    return NIG
+
 
 def main():
     print "KD-Mean algorithm"
     random.seed(5)
-    dataset = random.rand(1000, 2)
-    #plt.plot(dataset[:,0], dataset[:,1], 'o')
-    #plt.show()
-    kmeans(dataset, 5)
+    # dataset = random.rand(1000, 2)
+    dataset = loadtxt('C:\\Users\\bbphuc\\Desktop\\UCI_Handwriten\\pendigits.tra', delimiter=',')
+    targets = dataset[:,-1]
+    dataset = dataset[:,:-1]
+    # plt.plot(dataset[:,0], dataset[:,1], 'o')
+    # plt.show()
+    outputs = kmeans(dataset, 10)
+    calcInformationGain(dataset, outputs, targets)
 
 if __name__ == '__main__':
     main()
